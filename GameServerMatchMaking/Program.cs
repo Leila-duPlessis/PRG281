f5f2077121ed5664f9a1cec2f3b49d304b1a00e6
Nhlamulo Ngwana <abraham.ngwana@gmail.com>
2026-08-16 18:29:20 +0200

    Added Events classes (MatchmakingEvent, NotificationEvent, MatchmakingEvent Manger) and 2 methods to subscribe too (match found and Notification) and a practical example in the main program to see if it works.

--- GameServerMatchMaking/Program.cs
+++ GameServerMatchMaking/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GameServerMatchMaking.Events.MatchMakingEvents;
 
 namespace GameServerMatchMaking
 {
@@ -10,6 +11,33 @@ namespace GameServerMatchMaking
     {
         static void Main(string[] args)
         {
+
+            //
+            //===========Example use of the events, it works!!!!!! ========
+
+            // 1.  Create an instance
+            MatchmakingEventManager eventManager = new MatchmakingEventManager();
+
+            // 2. Subscribe to event
+            eventManager.OnMatchFound += (sender, e) =>
+            {
+                Console.WriteLine($"[MATCH CREATED] ID: {e.MatchId} on Server {e.ServerIp} with {e.PlayerCount} players!");
+            };
+
+            // 3. Fire the event!
+            eventManager.MatchFound("MATCH_901", "10.0.2.1", 8);// 1. Creates instance
+            MatchmakingEventManager EventManager1 = new MatchmakingEventManager();
+
+            // 2. Subscribe to your event using '+='
+            EventManager1.OnMatchFound += (sender, e) =>
+            {
+                Console.WriteLine($"[MATCH CREATED] ID: {e.MatchId} on Server {e.ServerIp} with {e.PlayerCount} players!");
+            };
+
+            // 3. Fire the event!
+            eventManager.MatchFound("MATCH_901", "10.0.2.1", 8);
+
+            Console.ReadLine();
         }
     }
 }
